777f46b6867d9b58bbae4920f324e65ab3096880
Crespone <crework96@gmail.com>
2022-03-22 21:16:51 +0100

    first commit

---
+++ gitHubCallCExercise.bat
@@ -0,0 +1,8 @@
+cd C:\Users\New\Desktop\C_exercises
+git init 
+git status
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Crengineer/C_programs.git
+git push -u origin main
