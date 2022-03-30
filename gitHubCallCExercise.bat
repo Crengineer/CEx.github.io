517864a13f9ba76e00534eba0b4068bc12444262
Crespone <crework96@gmail.com>
2022-03-30 22:30:32 +0200

    second commit

--- gitHubCallCExercise.bat
+++ gitHubCallCExercise.bat
@@ -2,7 +2,7 @@ cd C:\Users\New\Desktop\C_exercises
 git init 
 git status
 git add .
-git commit -m "first commit"
+git commit -m "second commit"
 git branch -M main
 git remote add origin https://github.com/Crengineer/C_programs.git
 git push -u origin main
