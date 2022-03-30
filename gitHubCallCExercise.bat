2d85f8b618e98dd3fd18db6c6457f5ee15bbf53e
Crengineer <crespi.marco96@gmail.com>
2022-03-30 23:25:27 +0200

    first commit

--- gitHubCallCExercise.bat
+++
@@ -1,9 +0,0 @@
-cd C:\Users\New\Desktop\C_exercises
-git init 
-git status
-git add .
-git commit -m "first commit"
-git remote add origin  https://github.com/Crengineer/CEx.github.io.git
-git remote -v
-git branch -M main
-git push -u origin main
