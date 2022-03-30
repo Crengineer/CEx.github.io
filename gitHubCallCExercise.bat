58689c8739fc5c1e73765df973cd8d5c5736e6e7
Crespone <crework96@gmail.com>
2022-03-30 22:43:10 +0200

    first commit

--- gitHubCallCExercise.bat
+++ gitHubCallCExercise.bat
@@ -4,5 +4,5 @@ git status
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/Crengineer/CEx.github.io
+git remote add origin https://github.com/Crengineer/CEx.github.io.git
 git push -u origin main
