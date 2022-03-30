e89d35fce9fee6c92b68beaa08b9f93ef4df6490
Crespone <crework96@gmail.com>
2022-03-30 19:06:52 +0200

    first commit

--- Laboratory_4/Histograms.c
+++ Laboratory_4/Histograms.c
@@ -38,13 +38,81 @@ int minValue(int * array){
 }
 
 
+
+
 void main(){
-	int seq[MAX], i;
+	int seq[MAX], i,j, min, max;
 	
+	//Keyboard insert
 	printf("\nInsert a sequence of %d elements: \n", MAX);
 	for(i=0;i<MAX;i++){
 		scanf("%d", &seq[i]);
 	}
+	
+	//Min e Max
+	min=minValue(seq);
+	max=maxValue(seq);
+	
+	if(!(min< 0)){
+		min =0;
+	}
+	if(!(max > 0)){
+		max =0;
+	}
+	for(i=0; i<MAX; i++){
+		printf("---");
+	}
+	printf("\n");
+	for(i=max; i>=min; i--){
+		if(i<10){
+			printf("%d  |", i);
+		} else {
+			printf("%d | ", i);
+		}
+		
+		for(j=0;j<10;j++){
+			if(seq[j] >= 0 && i >= 0){
+				if(i == (int)average(seq) || i==0){
+				printf("---");
+			} else if(seq[j] >= i){
+				printf(" * ");
+			} else {
+				printf("   ");
+			}
+			} else if(seq[j] < 0 && i <= 0) {
+			if(i == (int)average(seq) || i==0){
+				printf("---");
+			} else if(seq[j] <= i){
+				printf(" * ");
+			} else {
+				printf("   ");
+			}
+			}
+			
+		}
+		printf("\n");
+	}
+	printf("    ");
+	for(i=0; i<MAX; i++){
+		printf("---");
+	}
+	printf("\n");
+	max=0;
+	for(i=0; i<2; i++){
+		printf("   ");
+		for(j=0;j<MAX;j++){
+			if(i==0){
+				max++;
+				printf(" %d ", max);
+			}
+			if(i==1){
+				printf(" %d ", seq[j]);
+			}
+		}
+		printf("\n");
+	}
+	
+	
 	
 	//printf("\n%lf", average(seq));
 	//printf("\n%d", maxValue(seq));
